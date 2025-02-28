7f375af68320a3c513e732b3403012b0564f3c8c
Divye Maggo <divye.maggo@flinders.edu.au>
2025-02-28 17:39:15 +1100

    Code Editting and Verification

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Tutorial to host vue app on github pages!!" />
     </div>
   </header>
 
